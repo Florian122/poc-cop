102cf2c170f83fb0f9e9459de618d9c60f0a505a
unknown <FlorianSchmitz1202@gmail.com>
2020-07-31 08:54:33 +0200

    Updates

---
+++ precache-manifest.53b7a3725b814b816f0f48018c38f192.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b3a72422a1c2cfb110a927af2de683",
+    "url": "/poc-cop/index.html"
+  },
+  {
+    "revision": "e0ee2c7ce4fd595fb78e",
+    "url": "/poc-cop/static/css/main.65e9811d.chunk.css"
+  },
+  {
+    "revision": "2aeaa61ca275457ec745",
+    "url": "/poc-cop/static/js/2.832b8c15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poc-cop/static/js/2.832b8c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ee2c7ce4fd595fb78e",
+    "url": "/poc-cop/static/js/main.18adc10c.chunk.js"
+  },
+  {
+    "revision": "909236ba4fda6f900450",
+    "url": "/poc-cop/static/js/runtime-main.ec54b679.js"
+  },
+  {
+    "revision": "1e36ee6d50c037b1bb4bdd3effa7a28e",
+    "url": "/poc-cop/static/media/Muli-Bold.1e36ee6d.ttf"
+  },
+  {
+    "revision": "df7330254513d2fa2f4c1e9ee98cc6c6",
+    "url": "/poc-cop/static/media/Muli.df733025.ttf"
+  }
+]);
